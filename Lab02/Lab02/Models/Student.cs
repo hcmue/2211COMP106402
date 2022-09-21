2058d122fa0cc73f53fb0027490abcb819a740b8
Hien Luong <hyhien@gmail.com>
2022-09-21 07:24:26 +0700

    Create model Student

---
+++ Lab02/Lab02/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab02.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public float Mark { get; set; }
+        public string Grade
+        {
+            get
+            {
+                if (Mark >= 8.5) return "A";
+                if (Mark >= 7.8) return "B";
+                if (Mark >= 5) return "C";
+                return "D";
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 2211COMP106402
 Công nghệ NET lớp thứ 4
+
+## Project sử dụng .NET Core 6
+
+## LAB 02 - Truyền và nhận dữ liệu giữa controller vs View
