611a6428e04b43119998b834ad921b2a15ecf0cf
Hien Luong <hyhien@gmail.com>
2022-09-21 08:27:58 +0700

    Add validation for Student Model

--- Lab02/Lab02/Models/Student.cs
+++ Lab02/Lab02/Models/Student.cs
@@ -1,10 +1,23 @@
-﻿namespace Lab02.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab02.Models
 {
     public class Student
     {
+        [DisplayName("Mã sinh viên")]
+        [MaxLength(10, ErrorMessage ="Tối đa 10 kí tự")]
+        [RegularExpression(@"\d{2}0110[34]\d{3}", ErrorMessage ="Chưa đúng định dạng mã sinh viên")]
         public string Id { get; set; }
+
+        [DisplayName("Họ tên sinh viên")]
         public string FullName { get; set; }
+
+        [DisplayName("Điểm sinh viên")]
+        [Range(0, 10, ErrorMessage ="Điểm từ 0 đến 10")]
         public float Mark { get; set; }
+
+        [DisplayName("Loại")]
         public string Grade
         {
             get
