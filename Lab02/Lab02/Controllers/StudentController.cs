5095d1a9fbf7608c189739b422253e80d72c6a2d
Hien Luong <hyhien@gmail.com>
2022-09-21 08:03:40 +0700

    Process save json file

--- Lab02/Lab02/Controllers/StudentController.cs
+++ Lab02/Lab02/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lab02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace Lab02.Controllers
 {
@@ -7,6 +9,20 @@ namespace Lab02.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        
+        string TxtFullPath => Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Student.txt");
+        string JsonFullPath => Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Student.json");
+
+        [HttpPost]
+        public IActionResult Index(Student sinhVien, string buttonLuu)
+        {
+            if (buttonLuu == "Lưu file JSON")
+            {
+                var jsonString = JsonSerializer.Serialize(sinhVien);
+                System.IO.File.WriteAllText(JsonFullPath, jsonString);
+            }
+            return View(sinhVien);
         }
     }
 }
