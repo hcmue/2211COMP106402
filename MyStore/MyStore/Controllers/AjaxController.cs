3f165b592a1c10deb0b53a3735f697828b1b16e7
Hien Luong <hyhien@gmail.com>
2022-11-09 08:00:46 +0700

    Demo - Ajax lay gio

---
+++ MyStore/MyStore/Controllers/AjaxController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyStore.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public IActionResult MyPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string ServerTime()
+        {
+            return DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
